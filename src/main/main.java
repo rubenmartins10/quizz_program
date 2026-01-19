f13ebdf558526bbeccc13791bf6ce2bc01d71d49
rubenmartins10 <rubendavidsilvamartins@gmail.com>
2026-01-19 13:05:20 +0000

    Quizz atualizado - Podemos melhorar mais e atualizar com mais perguntas

--- src/main/main.java
+++ src/main/main.java
@@ -12,28 +12,53 @@ public class main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		Question q1 = new Question();
-		q1.frase = "Qual é a capital da França"; //cria-se o Q1 e preenchemos os dados dele
-		q1.opcoes = new String[] {"Lisboa", "Porto", "Paris"}; //criar o array 
-		q1.correctResponse = 2;
+		Question[] perguntas = new Question[4];//dizemos que o nosso quizz tem espaço para 4 perguntas
 		
-		int respostaDoUser = JOptionPane.showOptionDialog(
-                null, 
-                q1.frase,//usa a frase guardada no objeto
-                "Quiz Java", 
-                JOptionPane.DEFAULT_OPTION, 
-                JOptionPane.QUESTION_MESSAGE, 
-                null, 
-                q1.opcoes,//usa as opções guardadas no objeto
-                q1.opcoes[0]);
+		perguntas[0] = new Question();
+		perguntas[0].frase = "Diz-me qual é a capital de Portugal?";
+		perguntas[0].opcoes = new String [] {"Porto", "Lisboa", "Berlim", "Aveiro"};
+		perguntas[0].correctResponse = 1; //lisboa
 		
+		perguntas[1] = new Question();
+		perguntas[1].frase = "Diz-me qual é a capital da França?";
+		perguntas[1].opcoes = new String [] {"Paris", "Lisboa", "Berlim", "Amsterdão"};
+		perguntas[1].correctResponse = 0; //Paris
 		
-		if (q1.isCorrect(respostaDoUser) == true) {
-            JOptionPane.showMessageDialog(null, "MUITO BEM! Acertaste.");
-        } else {
-            JOptionPane.showMessageDialog(null, "Errado... Tenta novamente!");
-        }
+		perguntas[2] = new Question();
+		perguntas[2].frase = "Diz-me qual é a capital de Espanha?";
+		perguntas[2].opcoes = new String [] {"Porto", "Lisboa", "Berlim", "Madrid"};
+		perguntas[2].correctResponse = 3; //Madrid
 		
+		perguntas[3] = new Question();
+		perguntas[3].frase = "Diz-me qual é a capital de Holanda?";
+		perguntas[3].opcoes = new String [] {"Porto", "Berlim", "Amsterdão", "Madrid"};
+		perguntas[3].correctResponse = 2; //Amsterdão
+		
+		int score = 0;
+		
+		for (int i = 0; i < perguntas.length; i++) {
+			
+			Question q = perguntas[i]; //pergunta atual, vê-se no índice i
+		
+			int respostaDoUser = JOptionPane.showOptionDialog(
+	                null, 
+	                q.frase,//usa a frase guardada no objeto
+	                "Quiz Java", 
+	                JOptionPane.DEFAULT_OPTION, 
+	                JOptionPane.QUESTION_MESSAGE, 
+	                null, 
+	                q.opcoes,//usa as opções guardadas no objeto
+	                q.opcoes[0]);
+			
+			
+			if (q.isCorrect(respostaDoUser) == true) {
+	            JOptionPane.showMessageDialog(null, "MUITO BEM! Acertaste.");
+	            score ++;
+	        } else {
+	            JOptionPane.showMessageDialog(null, "Errado... Tenta novamente!");
+	        }
+		}
+		JOptionPane.showMessageDialog(null, "O jogo acabou e tiveste pontuação de " + score + " em " + perguntas.length);
 		
 		
 		sc.close();
